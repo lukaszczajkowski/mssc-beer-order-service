9fd519f82ff0463a428d37c5c1481e7cf4b54b79
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-08-22 17:56:31 +0200

    Testing failed validation

--- src/test/java/guru/sfg/beer/order/service/testcomponents/BeerOrderValidationListener.java
+++ src/test/java/guru/sfg/beer/order/service/testcomponents/BeerOrderValidationListener.java
@@ -19,14 +19,19 @@ public class BeerOrderValidationListener {
 
     @JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
     public void listen(Message msg) {
+        boolean isValid = true;
 
         ValidateOrderRequest request = (ValidateOrderRequest) msg.getPayload();
 
-        System.out.println("########## I RAN #############");
+        //condition to fail validation
+        if (request.getBeerOrder().getCustomerRef() != null
+                && request.getBeerOrder().getCustomerRef().equals("fail-validation")) {
+            isValid = false;
+        }
 
         jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESULT_QUEUE,
                 ValidateOrderResult.builder()
-                        .isValid(true)
+                        .isValid(isValid)
                         .orderId(request.getBeerOrder().getId())
                         .build());
     }
